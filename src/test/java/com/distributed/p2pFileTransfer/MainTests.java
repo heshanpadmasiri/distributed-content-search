ebcb2bf0e3cc8f9d17986b89afc54aa3a90374c5
KalanaDananjaya <kalana.16@cse.mrt.ac.lk>
2021-01-04 16:15:59 +0530

    Add unit tests for file search and download

---
+++ src/test/java/com/distributed/p2pFileTransfer/MainTests.java
@@ -0,0 +1,34 @@
+package com.distributed.p2pFileTransfer;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.file.Paths;
+import java.util.List;
+
+
+class MainTests {
+	/**
+	 * Download file from a node
+	 * @throws UnknownHostException
+	 */
+	@Test
+	void testFileDownload() throws UnknownHostException {
+		FileHandler fHandler = new FileHandler();
+		InetAddress add = InetAddress.getByName("127.0.0.1");
+		Node myNode = new Node(add, 8080);
+		fHandler.downloadFile(myNode,"sites.csv", Paths.get(""));
+	}
+
+	/**
+	 * Search for file in self storage
+	 */
+	@Test
+	void testFileSearch() {
+		FileHandler fHandler = new FileHandler();
+		List<String> matchingFiles = fHandler.searchForFile("s");
+		matchingFiles.forEach(System.out::println);
+	}
+
+}
