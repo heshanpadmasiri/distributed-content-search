1af81a9bde1612dbdfe6443c9bc120c84af1a51e
Heshan <hpheshan@gmail.com>
2021-01-09 12:24:37 +0530

    Implemented tests to send multiple search queries

--- src/test/java/com/distributed/p2pFileTransfer/QueryDispatcherTest.java
+++ src/test/java/com/distributed/p2pFileTransfer/QueryDispatcherTest.java
@@ -1,13 +1,17 @@
 package com.distributed.p2pFileTransfer;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
-import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -26,6 +30,7 @@ class QueryDispatcherTest {
     private DatagramSocket socket;
     private boolean terminate = false;
     private Node node;
+    private int messageCount = 0;
 
     public SocketListener(int port) throws SocketException {
       socket = new DatagramSocket(port);
@@ -39,8 +44,9 @@ class QueryDispatcherTest {
         DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
         try {
           socket.receive(incoming);
-          synchronized (lastMessage){
+          synchronized (lastMessage) {
             lastMessage = new String(buffer).split("\0")[0];
+            messageCount++;
           }
         } catch (IOException e) {
           throw new RuntimeException("IO exception in socket listener");
@@ -51,10 +57,18 @@ class QueryDispatcherTest {
 
     public String getLastMessage() {
       String message;
-      synchronized (lastMessage){
+      synchronized (lastMessage) {
         message = lastMessage;
       }
       return message;
+    }
+
+    public int getMessageCount() {
+      int count;
+      synchronized (lastMessage) {
+        count = messageCount;
+      }
+      return count;
     }
 
     public void stop() {
@@ -98,11 +112,11 @@ class QueryDispatcherTest {
   @Test
   void dispatchOne() {
     String[] messages = {
-            "0047 SER 129.82.62.142 5070 \"Lord of the rings\"",
-            "0027 JOIN 64.12.123.190 432",
-            "0028 LEAVE 64.12.123.190 432",
+      "0047 SER 129.82.62.142 5070 \"Lord of the rings\"",
+      "0027 JOIN 64.12.123.190 432",
+      "0028 LEAVE 64.12.123.190 432",
     };
-    for(String message: messages){
+    for (String message : messages) {
       Query query = Query.createQuery(message, socketListener.toNode());
       this.queryDispatcher.dispatchOne(query);
       try {
@@ -116,4 +130,25 @@ class QueryDispatcherTest {
     }
   }
 
+  @Test
+  void dispatchAllSearch() {
+    List<String> messages =
+        Stream.of(
+                "0047 SER 129.82.62.142 5070 \"Lord of the rings1\"",
+                "0047 SER 129.82.62.142 5070 \"Lord of the rings2\"",
+                "0047 SER 129.82.62.142 5070 \"Lord of the rings3\"")
+            .collect(Collectors.toList());
+    List<Query> queries = Query.createQuery(messages, socketListener.toNode());
+    queryDispatcher.dispatchAll(queries);
+    try {
+      TimeUnit.SECONDS.sleep(1);
+      String last = socketListener.getLastMessage();
+      int count = socketListener.getMessageCount();
+      assertNotNull(last);
+      assertTrue(messages.contains(last));
+      assertEquals(queries.size(),count);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
 }
