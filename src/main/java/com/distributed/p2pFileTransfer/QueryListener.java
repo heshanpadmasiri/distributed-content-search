2e1014eef55c1e27b1f7a8c07e1bbc0cf5980ccf
Heshan <hpheshan@gmail.com>
2021-01-19 11:02:11 +0530

    Modified query listener to call file download function

--- src/main/java/com/distributed/p2pFileTransfer/QueryListener.java
+++ src/main/java/com/distributed/p2pFileTransfer/QueryListener.java
@@ -5,6 +5,9 @@ import java.net.*;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 class QueryListener implements Runnable {
   private final AbstractFileTransferService fileTransferService;
@@ -12,8 +15,10 @@ class QueryListener implements Runnable {
   private DatagramSocket socket;
   private boolean terminate = false;
   private HashMap<Node, List<Executor>> pendingExecutors;
+  private Logger logger;
 
-  public QueryListener(AbstractFileTransferService fileTransferService, int port) throws SocketException {
+  public QueryListener(AbstractFileTransferService fileTransferService, int port)
+      throws SocketException {
     this.fileTransferService = fileTransferService;
     executorService = Executors.newCachedThreadPool();
     pendingExecutors = new HashMap<>();
@@ -21,6 +26,7 @@ class QueryListener implements Runnable {
     socket.setReuseAddress(true);
     socket.bind(new InetSocketAddress(port));
     socket.setSoTimeout(1000);
+    logger = Logger.getLogger(this.getClass().getName());
   }
 
   public DatagramSocket getSocket() {
@@ -44,7 +50,7 @@ class QueryListener implements Runnable {
         throw new RuntimeException("IO exception in socket listener");
       }
     }
-    while (socket.isBound()){
+    while (socket.isBound()) {
       socket.disconnect();
       socket.close();
     }
@@ -52,48 +58,72 @@ class QueryListener implements Runnable {
 
   /**
    * Used by executors to tell query listener to notify them when a message is received from a node
+   *
    * @param node node from which executor is expecting a message
    * @param executor who is expecting the message
    */
-  public void registerForResponse(Node node, Executor executor){
-    if (pendingExecutors.containsKey(node)){
+  public void registerForResponse(Node node, Executor executor) {
+    if (pendingExecutors.containsKey(node)) {
       pendingExecutors.get(node).add(executor);
     } else {
-        pendingExecutors.put(node, new LinkedList<>(Collections.singletonList(executor)));
+      pendingExecutors.put(node, new LinkedList<>(Collections.singletonList(executor)));
     }
   }
 
   /**
    * Used by executors to unregister from future responses from a node
+   *
    * @param node node to which it previously registered to
    * @param executor who wants to stop notifications
    */
-  public void unRegisterForResponse(Node node, Executor executor){
+  public void unRegisterForResponse(Node node, Executor executor) {
     pendingExecutors.get(node).remove(executor);
   }
-
 
   public void stop() {
     terminate = true;
   }
 
-  private class ListenerThread implements Runnable{
+  private class ListenerThread implements Runnable {
     String message;
     Node origin;
+    FileHandler fileHandler;
 
     public ListenerThread(String message, Node origin) {
       this.message = message;
       this.origin = origin;
+      fileHandler = fileTransferService.getFileHandler();
     }
 
     @Override
     public void run() {
       String queryType = message.split(" ")[1];
-      switch (queryType){
+      switch (queryType) {
         case "SEROK":
-         pendingExecutors.get(origin).forEach((executor) -> {
-           executor.notify(message);
-         });
+          pendingExecutors
+              .get(origin)
+              .forEach(
+                  (executor) -> {
+                    executor.notify(message);
+                  });
+          String[] data = message.split(" ");
+          int numberOfFiles = Integer.parseInt(data[2]);
+          if (numberOfFiles > 0) {
+            try {
+              Node source = new Node(InetAddress.getByName(data[3]), Integer.parseInt(data[4]));
+              Stream.of(data)
+                  .skip(4)
+                  .forEach(
+                      fileName -> {
+                        fileHandler.downloadFileToCache(source, fileName);
+                      });
+            } catch (UnknownHostException e) {
+              logger.log(
+                  Level.WARNING,
+                  String.format(
+                      "Failed to get Inet address ipAddress: %s port: %s", data[3], data[4]));
+            }
+          }
           break;
         default:
           throw new IllegalStateException("Unexpected value: " + queryType);
@@ -101,4 +131,3 @@ class QueryListener implements Runnable {
     }
   }
 }
-
