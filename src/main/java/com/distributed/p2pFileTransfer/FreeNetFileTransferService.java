c4eec455db64668e6e3c91fe961be321104bd5ce
Heshan <hpheshan@gmail.com>
2021-01-24 15:24:31 +0530

    Implemented code for file search

--- src/main/java/com/distributed/p2pFileTransfer/FreeNetFileTransferService.java
+++ src/main/java/com/distributed/p2pFileTransfer/FreeNetFileTransferService.java
@@ -2,15 +2,18 @@ package com.distributed.p2pFileTransfer;
 
 import java.net.SocketException;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.Properties;
-import java.util.concurrent.Future;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FreeNetFileTransferService extends AbstractFileTransferService {
 
   private static FreeNetFileTransferService instance;
+  private ExecutorService executorService;
 
-  public static synchronized FreeNetFileTransferService getInstance(Properties configuration) throws SocketException {
+  public static synchronized FreeNetFileTransferService getInstance(Properties configuration)
+      throws SocketException {
     if (instance == null) {
       // todo: replace with concrete implementation
       Network network = null;
@@ -26,16 +29,50 @@ public class FreeNetFileTransferService extends AbstractFileTransferService {
     return instance;
   }
 
-  private FreeNetFileTransferService(
-      Network network,
-      FileHandler fileHandler,
-      int port) throws SocketException {
+  private FreeNetFileTransferService(Network network, FileHandler fileHandler, int port)
+      throws SocketException {
     super(network, fileHandler, port);
+    this.executorService = Executors.newCachedThreadPool();
   }
 
   @Override
   public Future<List<String>> searchForFile(String query) {
-    return null;
+    Callable<List<String>> searchExecutor =
+        () -> {
+          List<Node> neighbours = new LinkedList<>();
+          this.getNetwork().getNeighbours().forEachRemaining(neighbours::add);
+          List<Query> queries =
+              Query.createQuery(this.getCommandBuilder().getSearchCommand(query), neighbours);
+          List<QueryResult> results =
+              this.getQueryDispatcher().dispatchAll(queries).stream()
+                  .map(
+                      each -> {
+                        try {
+                          return each.get();
+                        } catch (InterruptedException e) {
+                          e.printStackTrace();
+                        } catch (ExecutionException e) {
+                          e.printStackTrace();
+                        }
+                        return null;
+                      })
+                  .collect(Collectors.toList());
+          Set<String> files = new TreeSet<>();
+          results.forEach(
+              result -> {
+                String[] data = result.getBody().split(" ");
+                int fileCount = Integer.parseInt(data[2]);
+                if (fileCount > 0) {
+                  IntStream.range(6, 6 + fileCount)
+                      .forEach(
+                          idx -> {
+                            files.add(data[idx]);
+                          });
+                }
+              });
+          return new ArrayList<>(files);
+        };
+    return executorService.submit(searchExecutor);
   }
 
   @Override
