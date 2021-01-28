c0351f4839cb675ca00681b8e3c6a7fbda441c1b
Heshan <hpheshan@gmail.com>
2021-01-28 13:05:44 +0530

    fixed test cache space

--- src/main/java/com/distributed/p2pFileTransfer/EndPointController.java
+++ src/main/java/com/distributed/p2pFileTransfer/EndPointController.java
@@ -15,66 +15,74 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URLConnection;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
 public class EndPointController {
 
-    private Storage fileStorage;
+  private Storage fileStorage;
 
-    @PostConstruct
-    public void initialize(){
-        fileStorage = new Storage("/home/kalana/distributed/content/server/cache_storage", "/home/kalana/distributed/content/server/local_storage", 10000000, this.getClass().getName());
+  @PostConstruct
+  public void initialize() {
+
+    Path currentRelativePath = Paths.get("");
+    Path serverCachePath = Paths.get(currentRelativePath.toString(), "server", "cache");
+    Path serverLocalPath = Paths.get(currentRelativePath.toString(), "server", "local");
+    fileStorage =
+        new Storage(
+            serverCachePath.toString(),
+            serverLocalPath.toString(),
+            10000000,
+            this.getClass().getName());
+  }
+
+  /**
+   * Serve the requested File
+   *
+   * @param res response
+   * @param fileName Name of the file
+   * @return ResponseEntity with File
+   * @throws IOException
+   */
+  @RequestMapping("/file/{name:.+}")
+  public ResponseEntity<Resource> downloadFile(
+      HttpServletResponse res, @PathVariable("name") String fileName) throws IOException {
+
+    System.out.println("Attempting to download " + fileName);
+    File file = fileStorage.getFile(fileName);
+
+    String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+    if (mimeType == null) {
+      // unknown mimetype so set the mimetype to application/octet-stream
+      mimeType = "application/octet-stream";
     }
+    res.setContentType(mimeType);
+    res.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
+    res.setContentLength((int) file.length());
 
-    /**
-     * Serve the requested File
-     *
-     * @param res      response
-     * @param fileName Name of the file
-     * @return ResponseEntity with File
-     * @throws IOException
-     */
-    @RequestMapping("/file/{name:.+}")
-    public ResponseEntity<Resource> downloadFile(HttpServletResponse res, @PathVariable("name") String fileName) throws IOException {
+    InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
 
-        System.out.println("Attempting to download " + fileName);
-        File file = fileStorage.getFile(fileName);
+    String hexHash = fileStorage.getFileHash(fileName);
+    res.setHeader("Hash", hexHash);
 
-        String mimeType = URLConnection.guessContentTypeFromName(file.getName());
-        if (mimeType == null) {
-            //unknown mimetype so set the mimetype to application/octet-stream
-            mimeType = "application/octet-stream";
-        }
-        res.setContentType(mimeType);
-        res.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
-        res.setContentLength((int) file.length());
+    System.out.println("Serving file from the server");
+    return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM).body(resource);
+  }
 
-        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
-
-        String hexHash = fileStorage.getFileHash(fileName);
-        res.setHeader("Hash", hexHash);
-
-        System.out.println("Serving file from the server");
-        return ResponseEntity.ok()
-                .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(resource);
-
-    }
-
-    /**
-     * Search for the requested file in self storage
-     *
-     * @param res      Response
-     * @param fileName Name of the file
-     * @return ResponseEntity Filenames that inclue the search query
-     */
-    @RequestMapping("/search/{name:.+}")
-    public ResponseEntity<String> searchFile(HttpServletResponse res, @PathVariable("name") String fileName) {
-        List<String> matchingNames = fileStorage.searchForFile(fileName);
-        String json = new Gson().toJson(matchingNames);
-        return ResponseEntity.ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(json);
-    }
+  /**
+   * Search for the requested file in self storage
+   *
+   * @param res Response
+   * @param fileName Name of the file
+   * @return ResponseEntity Filenames that inclue the search query
+   */
+  @RequestMapping("/search/{name:.+}")
+  public ResponseEntity<String> searchFile(
+      HttpServletResponse res, @PathVariable("name") String fileName) {
+    List<String> matchingNames = fileStorage.searchForFile(fileName);
+    String json = new Gson().toJson(matchingNames);
+    return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(json);
+  }
 }
--- src/test/java/com/distributed/p2pFileTransfer/MainTests.java
+++ src/test/java/com/distributed/p2pFileTransfer/MainTests.java
@@ -1,64 +1,105 @@
 package com.distributed.p2pFileTransfer;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 class MainTests {
-    
-    String clientCacheDir = "/home/kalana/distributed/content/node/cache_storage";
-    String clientLocalDir = "/home/kalana/distributed/content/node/local_storage";
-    long cacheSize = 10000000;
-    String clientPort = "9000";
 
-    String serverCacheDir = "/home/kalana/distributed/content/server/cache_storage";
-    String serverLocalDir = "/home/kalana/distributed/content/server/local_storage";
-    String serverPort = "7000";
+  static String clientCacheDir = "/home/kalana/distributed/content/node/cache_storage";
+  static String clientLocalDir = "/home/kalana/distributed/content/node/local_storage";
+  long cacheSize = 10000000;
+  String clientPort = "9000";
 
-    @BeforeEach
-    public void setUp() {
-        FileHandler server = new FileHandler(serverCacheDir, serverLocalDir, cacheSize, serverPort);
+  static String serverCacheDir = "/home/kalana/distributed/content/server/cache_storage";
+  static String serverLocalDir = "/home/kalana/distributed/content/server/local_storage";
+  String serverPort = "7000";
+
+  static Path clientPath, serverPath;
+
+  @BeforeEach
+  public void setUp() {
+    FileHandler server = new FileHandler(serverCacheDir, serverLocalDir, cacheSize, serverPort);
+  }
+
+  /**
+   * Download file from a node
+   *
+   * @throws UnknownHostException
+   */
+  @Test
+  void testFileDownload() throws UnknownHostException, InterruptedException {
+
+    FileHandler fHandler = new FileHandler(clientCacheDir, clientLocalDir, cacheSize, clientPort);
+    InetAddress add = InetAddress.getByName("127.0.0.1");
+    Node source = new Node(add, Integer.parseInt(serverPort));
+    fHandler.downloadFileToLocal(source, "sites.csv");
+    // fHandler.downloadFileToCache(source, "sites.csv");
+    // Sleep is added to wait till the file download is completed before exiting
+    Thread.sleep(2000);
+  }
+
+  /** Search for file in self storage */
+  @Test
+  void testFileSearch() {
+    FileHandler fHandler = new FileHandler(clientCacheDir, clientLocalDir, cacheSize, clientPort);
+    List<String> matchingFiles = fHandler.searchForFile("s");
+    matchingFiles.forEach(System.out::println);
+  }
+
+  /** Create required space in the cache storage */
+  @Test
+  void testCacheSpace() {
+    FileHandler fHandler = new FileHandler(clientCacheDir, clientLocalDir, cacheSize, clientPort);
+    fHandler.makeCacheSpace(3000000);
+  }
+
+  @BeforeAll
+  static void beforeAll() throws IOException {
+    Path currentRelativePath = Paths.get("");
+    clientPath = Paths.get(currentRelativePath.toString(), "node");
+    serverPath = Paths.get(currentRelativePath.toString(), "server");
+    clientPath.toFile().mkdir();
+    serverPath.toFile().mkdir();
+
+    Path clientCachePath = Paths.get(currentRelativePath.toString(), "node", "cache");
+    Path clientLocalPath = Paths.get(currentRelativePath.toString(), "node", "local");
+    Path serverCachePath = Paths.get(currentRelativePath.toString(), "server", "cache");
+    Path serverLocalPath = Paths.get(currentRelativePath.toString(), "server", "local");
+    Path serverDummyFile = Paths.get(serverLocalPath.toString(), "sites.csv");
+    clientCacheDir = clientCachePath.toAbsolutePath().toString();
+    clientLocalDir = clientLocalPath.toAbsolutePath().toString();
+    serverCacheDir = serverCachePath.toAbsolutePath().toString();
+    serverLocalDir = serverLocalPath.toAbsolutePath().toString();
+    clientCachePath.toFile().mkdir();
+    clientLocalPath.toFile().mkdir();
+    serverCachePath.toFile().mkdir();
+    serverLocalPath.toFile().mkdir();
+    serverDummyFile.toFile().createNewFile();
+  }
+
+  @AfterAll
+  static void afterAll() {
+    deleteDir(clientPath.toFile());
+    deleteDir(serverPath.toFile());
+  }
+
+  private static void deleteDir(File dir) {
+    File[] contents = dir.listFiles();
+    if (contents != null) {
+      for (File f : contents) {
+        deleteDir(f);
+      }
     }
-
-
-    /**
-     * Download file from a node
-     *
-     * @throws UnknownHostException
-     */
-    @Test
-    void testFileDownload() throws UnknownHostException, InterruptedException {
-
-
-        FileHandler fHandler = new FileHandler(clientCacheDir, clientLocalDir, cacheSize, clientPort);
-        InetAddress add = InetAddress.getByName("127.0.0.1");
-        Node source = new Node(add, Integer.parseInt(serverPort));
-        fHandler.downloadFileToLocal(source, "sites.csv");
-        //fHandler.downloadFileToCache(source, "sites.csv");
-        // Sleep is added to wait till the file download is completed before exiting
-        Thread.sleep(2000);
-    }
-
-    /**
-     * Search for file in self storage
-     */
-    @Test
-    void testFileSearch() {
-        FileHandler fHandler = new FileHandler(clientCacheDir, clientLocalDir, cacheSize, clientPort);
-        List<String> matchingFiles = fHandler.searchForFile("s");
-        matchingFiles.forEach(System.out::println);
-    }
-
-    /**
-     * Create required space in the cache storage
-     */
-    @Test
-    void testCacheSpace() {
-        FileHandler fHandler = new FileHandler(clientCacheDir, clientLocalDir, cacheSize, clientPort);
-        fHandler.makeCacheSpace(3000000);
-    }
-
+    dir.delete();
+  }
 }
